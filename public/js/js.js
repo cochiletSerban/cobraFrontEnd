452b641e030a255215e697acd8054889741d7042
cochiletSerban <cochiletSerban@gmail.com>
2018-10-18 20:15:36 +0300

    fixed coding style issues

--- public/js/js.js
+++ public/js/js.js
@@ -1,41 +1,37 @@
 // make socket connection
 var socket = io.connect('https://cobras.herokuapp.com/')
 
-
 $(document).ready(() => {
-  //event on send
-  var msg = $("#message");
-  var handle = $("#handle");
-  var btn = $("#send");
-  var out = $("#output");
-  var feed = $("#feedback");
+  // event on send
+  var msg = $('#message')
+  var handle = $('#handle')
+  var btn = $('#send')
+  var out = $('#output')
+  var feed = $('#feedback')
 
   btn.click(() => {
-    socket.emit('chatMsg',{
+    socket.emit('chatMsg', {
       message: msg.val(),
       handle: handle.val()
     })
   })
 
   msg.keydown(() => {
-    socket.emit('typing',{
+    socket.emit('typing', {
       name: handle.val()
-    });
-  });
+    })
+  })
 
   socket.on('chatMsg', (data) => {
-    feed.html(" ");
+    feed.html('')
     out.append('<p><strong>' + data.handle + ': </strong>' +
-    data.message + '</p>');
-  });
+    data.message + '</p>')
+  })
 
-  socket.on('typing',(data)=>{
-    feed.html('<p><em>' + data.name + ' is typing a message...</em></p>');
+  socket.on('typing', (data) => {
+    feed.html('<p><em>' + data.name + ' is typing a message...</em></p>')
     setTimeout(() => {
-        feed.html("");
-    },1000)
-  });
-
-
-
-});
+      feed.html('')
+    }, 1000)
+  })
+})
