42534d74f577fcd550abe9006b00bc2e8fddddba
cochiletSerban <cochiletSerban@gmail.com>
2018-11-04 23:40:15 +0200

    added hosted backend

--- public/js/js.js
+++ public/js/js.js
@@ -1,9 +1,9 @@
 // make socket connection to geroku backend used for poduction ( when pushing to master)
 // comment this line for local development ( if you are runnig the back end locally )
- //var socket = io.connect('https://cobras.herokuapp.com/')
+var socket = io.connect('https://cobras.herokuapp.com/')
 
 // used for local development , uncommetn for local dev
-const socket = io.connect('http://localhost:3000/')
+//const socket = io.connect('http://localhost:3000/')
 
 // inits
 let selectedCards = [], cardList = [], myId = ''
